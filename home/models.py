12c0ac5b6db2ebf34f9bbfca0d9c22300ba56653
Alex Clark <aclark@aclark.net>
2023-03-18 10:13:46 -0400

    Update

--- home/models.py
+++ home/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 from wagtail.models import Page
 
 
 class HomePage(Page):
     pass
+
+
+class TimeEntry(models.Model):
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(blank=True, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.description} ({self.start_time.strftime('%Y-%m-%d %H:%M:%S')})"
