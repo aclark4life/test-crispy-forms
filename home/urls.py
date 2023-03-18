2e78f6826d7eb7492b22c7e5a45c6d0dcb144471
Alex Clark <aclark@aclark.net>
2023-03-18 10:32:28 -0400

    Update

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('create/', views.create_time_entry, name='create_time_entry'),
-    path('<int:time_entry_id>/', views.view_time_entry, name='view_time_entry'),
-    path('<int:time_entry_id>/update/', views.update_time_entry, name='update_time_entry'),
-    path('<int:time_entry_id>/delete/', views.delete_time_entry, name='delete_time_entry'),
+    # path('<int:time_entry_id>/', views.view_time_entry, name='view_time_entry'),
+    # path('<int:time_entry_id>/update/', views.update_time_entry, name='update_time_entry'),
+    # path('<int:time_entry_id>/delete/', views.delete_time_entry, name='delete_time_entry'),
 ]
