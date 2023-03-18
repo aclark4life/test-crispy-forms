fa8c6cf81183a29b2c0a33640a2d1cdacee96003
Alex Clark <aclark@aclark.net>
2023-03-18 18:15:38 -0400

    Update

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import TimeEntryForm
+from .models import TimeEntry
+
+
+def index(request):
+    time_entries = TimeEntry.objects.all()
+    context = {'time_entries': time_entries}
+    return render(request, 'index.html', context)
+
 
 def create_time_entry(request):
     if request.method == 'POST':
